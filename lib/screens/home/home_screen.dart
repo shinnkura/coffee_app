3faf76a2cf966188f261313575b387e95e3414ef
shinnkura <n.shinzo1010@icloud.com>
2023-05-24 01:31:50 +0900

    ホットリロードできない問題の解決。アイコンを表示できな問題の浮上

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:coffee_app/constants.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,10 +9,13 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
-        // elevation: 0,
-        // leading: IconButton(
-        //     onPressed: onPressed,
-        //     icon: SvgPicture.asset("asetts/icons/menu.svg")),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          // icon: Image.asset("assets/icons/menu.png"),
+          // Todo: アイコンを表示できない問題を解決。そもそも、leadingウィジェットが機能しているか確認する。
+          icon: const ImageIcon(AssetImage("assets/icons/menu.png")),
+        ),
       ),
     );
   }
