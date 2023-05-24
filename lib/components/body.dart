461b9c8944977e6e00633a0cfee548fe1e4767c0
shinnkura <n.shinzo1010@icloud.com>
2023-05-24 12:32:15 +0900

    検索窓の制作。ToDo:虫眼鏡アイコンの追加

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -23,7 +23,47 @@ class Body extends StatelessWidget {
                     bottomRight: Radius.circular(36),
                   ),
                 ),
-              )
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(.23),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: kPrimaryColor.withOpacity(.5),
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            // suffixIcon: Icon(Icons.search),
+                          ),
+                        ),
+                      ),
+                      Icon(Icons.search),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         )
