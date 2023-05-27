1b142868f18394c330f0f9da1e8fa7e0eae19405
shinnkura <n.shinzo1010@icloud.com>
2023-05-27 13:14:02 +0900

    header_with_searchbox.dartをcomponentsに新しく作った

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,7 @@
-import 'package:coffee_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:coffee_app/constants.dart';
+
+import 'header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
   // const body({super.key});
@@ -8,100 +10,9 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[HeaderWithSearchBox(size: size)],
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    super.key,
-    required this.size,
-  });
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: size.height * .2,
-      child: Stack(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              left: kDefaultPadding,
-              right: kDefaultPadding,
-              bottom: 36 + kDefaultPadding,
-            ),
-            height: size.height * .2 - 20,
-            decoration: const BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  'test',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Spacer(),
-                Image.asset(
-                  "assets/icons/logo.png",
-                  height: 100,
-                  width: 50,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        // suffixIcon: Icon(Icons.search),
-                      ),
-                    ),
-                  ),
-                  Icon(
-                    Icons.search,
-                    color: kPrimaryColor.withOpacity(.5),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[HeaderWithSearchBox(size: size)],
       ),
     );
   }
---
+++ lib/components/header_with_searchbox.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_app/constants.dart';
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * .2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * .2 - 20,
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Hello User!',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                Spacer(),
+                Image.asset(
+                  "assets/icons/logo.png",
+                  height: 100,
+                  width: 50,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        // suffixIcon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Icons.search,
+                    color: kPrimaryColor.withOpacity(.5),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
