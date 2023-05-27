ad8c2ca71e240fe409bbd6e98bf806d0cc674e37
shinnkura <n.shinzo1010@icloud.com>
2023-05-27 19:14:24 +0900

    TitleWithCustomUnderlineウィジェットを作成

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -12,7 +12,48 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
-        children: <Widget>[HeaderWithSearchBox(size: size)],
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithCustomUnderline(),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          const Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              "Recommended",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
       ),
     );
   }
