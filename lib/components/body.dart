8451afaeadeb9b4b1f34f2a3f5c5e7798471ccd1
shinnkura <n.shinzo1010@icloud.com>
2023-05-29 11:20:44 +0900

    RecomendePlantCardウィジェットの作成。カートボタンの左側にエラーが発生（サイズがoverしてる）

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:coffee_app/components/title_with_more_bttn.dart';
 import 'package:flutter/material.dart';
 import 'package:coffee_app/constants.dart';
@@ -10,7 +12,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
@@ -19,54 +21,122 @@ class Body extends StatelessWidget {
             title: 'おすすめ',
             press: () {},
           ),
-          SizedBox(
-            width: size.width * .8,
+          Row(
+            children: [
+              RecomendePlantCard(
+                image: "assets/images/image_1.png",
+                title: "BlueMouten",
+                price: 1000,
+                press: () {},
+              ),
+              RecomendePlantCard(
+                image: "assets/images/image_1.png",
+                title: "BlueMouten",
+                price: 1000,
+                press: () {},
+              ),
+              RecomendePlantCard(
+                image: "assets/images/image_1.png",
+                title: "BlueMouten",
+                price: 1000,
+                press: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecomendePlantCard extends StatelessWidget {
+  const RecomendePlantCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.price,
+    required this.press,
+  });
+
+  final String image, title;
+  final int price;
+  final Function()? press;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return SizedBox(
+      width: size.width * .8,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(
+              left: kDefaultPadding,
+              top: kDefaultPadding / 2,
+              bottom: kDefaultPadding * 2.5,
+            ),
+            height: 250,
+            width: 150,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  offset: const Offset(0, 10),
+                  blurRadius: 50,
+                  color: kPrimaryColor.withOpacity(0.23),
+                ),
+              ],
+            ),
             child: Column(
               children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: kDefaultPadding,
-                    top: kDefaultPadding / 2,
-                    bottom: kDefaultPadding * 2.5,
-                  ),
-                  height: 200,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
+                GestureDetector(
+                  onTap: press,
+                  child: Container(
+                    height: 190,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          const BorderRadius.vertical(top: Radius.circular(16)),
+                      image: DecorationImage(
+                        image: AssetImage(image),
+                        fit: BoxFit.cover,
                       ),
-                    ],
+                    ),
                   ),
-                  child: Column(
+                ),
+                Expanded(
+                  child: Row(
                     children: [
                       Container(
-                        height: 170,
-                        decoration: const BoxDecoration(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(16)),
-                          image: DecorationImage(
-                            image: AssetImage("assets/images/image_1.png"),
-                            fit: BoxFit.cover,
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                        ),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "$title\n".toUpperCase(),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "\$$price",
+                                style: const TextStyle(
+                                  color: kPrimaryColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                      const Expanded(
-                        // child: Container(
-                        //   padding: const EdgeInsets.only(
-                        //       bottom: kDefaultPadding / 2),
-                        // alignment: Alignment.center,
-                        child: Text(
-                          'カフェラテ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add_shopping_cart),
                       ),
                     ],
                   ),
