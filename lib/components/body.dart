bdd86b89d27092b231da695b0c50a35cffea2b39
shinnkura <n.shinzo1010@icloud.com>
2023-06-04 16:40:42 +0900

    recomend_coffee.dartファイルを作成

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:coffee_app/components/recomend_coffee.dart';
 import 'package:coffee_app/components/title_with_more_bttn.dart';
 import 'package:flutter/material.dart';
 import 'package:coffee_app/constants.dart';
@@ -21,133 +22,7 @@ class Body extends StatelessWidget {
             title: 'おすすめ',
             press: () {},
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                RecomendePlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "BlueMouten",
-                  price: 1000,
-                  press: () {},
-                ),
-                RecomendePlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "BlueMouten",
-                  price: 1000,
-                  press: () {},
-                ),
-                RecomendePlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "BlueMouten",
-                  price: 1000,
-                  press: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RecomendePlantCard extends StatelessWidget {
-  const RecomendePlantCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.price,
-    required this.press,
-  });
-
-  final String image, title;
-  final int price;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-
-    return SizedBox(
-      // width: size.width * .8,
-      height: size.height * .8,
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(
-              left: kDefaultPadding,
-              top: kDefaultPadding / 2,
-              bottom: kDefaultPadding * 2.5,
-            ),
-            height: 250,
-            width: 170,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  offset: const Offset(0, 10),
-                  blurRadius: 50,
-                  color: kPrimaryColor.withOpacity(0.23),
-                ),
-              ],
-            ),
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: press,
-                  child: Container(
-                    height: 190,
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          const BorderRadius.vertical(top: Radius.circular(16)),
-                      image: DecorationImage(
-                        image: AssetImage(image),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 10,
-                        ),
-                        child: RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: "$title\n".toUpperCase(),
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              TextSpan(
-                                text: "\$$price",
-                                style: const TextStyle(
-                                  color: kPrimaryColor,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.add_shopping_cart),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+          RecomendCoffee(),
         ],
       ),
     );
