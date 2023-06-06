9b82eb2438e0bd932036ac21d8f54a52752ddc90
shinnkura <n.shinzo1010@icloud.com>
2023-06-06 11:39:50 +0900

    icon_card.dartファイルを作成

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,10 +1,84 @@
+import 'package:coffee_app/screens/details/components/icon_card.dart';
 import 'package:flutter/material.dart';
+import 'package:coffee_app/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+            child: SizedBox(
+              height: size.height * 0.8,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: kDefaultPadding * 3,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: IconButton(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: kDefaultPadding,
+                              ),
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.arrow_back_ios,
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          const IconCard(
+                            icon: "assets/icons/umbrella.png",
+                          ),
+                          const IconCard(
+                            icon: "assets/icons/sun.png",
+                          ),
+                          const IconCard(
+                            icon: "assets/icons/temperature.png",
+                          ),
+                          const IconCard(
+                            icon: "assets/icons/ground.png",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                      height: size.height * 0.8,
+                      width: size.width * 0.75,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(63),
+                          bottomLeft: Radius.circular(63),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 60,
+                            color: kPrimaryColor.withOpacity(0.29),
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/image_1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/screens/details/components/icon_card.dart
@@ -0,0 +1,39 @@
+import 'package:coffee_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    super.key,
+    required this.icon,
+  });
+
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    // Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      padding: const EdgeInsets.all(kDefaultPadding / 2),
+      height: 62,
+      width: 62,
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 15),
+            blurRadius: 22,
+            color: kPrimaryColor.withOpacity(0.22),
+          ),
+          const BoxShadow(
+            offset: Offset(-15, -15),
+            blurRadius: 20,
+            color: Colors.white,
+          ),
+        ],
+      ),
+      child: Image.asset(icon),
+    );
+  }
+}
